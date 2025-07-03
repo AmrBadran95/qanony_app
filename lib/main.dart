6742cf6938bac1b46cfc94120374a728a4de4a9f
AlaaAyman <alaalilo789@gmail.com>
2025-07-03 10:43:01 +0300

    ''

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const QanonyApp());
@@ -12,9 +13,15 @@ class QanonyApp extends StatelessWidget {
     return MaterialApp(
       title: 'قانوني',
       theme: ThemeData(fontFamily: 'Cairo'),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       builder: (context, child) {
         return Directionality(textDirection: TextDirection.rtl, child: child!);
       },
+
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
