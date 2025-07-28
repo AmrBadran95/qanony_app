26aea183834b9dc82b34b0665dace6920f08028b
AlaaAyman <alaalilo789@gmail.com>
2025-07-28 10:04:22 +0300

    first edit in order screen

--- lib/presentation/screens/orders_lawyer_screen.dart
+++ lib/presentation/screens/orders_lawyer_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qanony/Core/styles/color.dart';
 import 'package:qanony/Core/styles/padding.dart';
 import 'package:qanony/Core/styles/text.dart';
 import 'package:qanony/Core/widgets/custom_button.dart';
 import 'package:qanony/Core/widgets/qanony_appointment_widget.dart';
+import 'package:qanony/data/models/order_status_enum.dart';
+import 'package:qanony/presentation/pages/my_appointments_tab.dart';
+import 'package:qanony/services/cubits/order_lawyer/order_cubit.dart';
 
 class OrdersLawyerScreen extends StatelessWidget {
   const OrdersLawyerScreen({super.key});
@@ -14,41 +18,99 @@ class OrdersLawyerScreen extends StatelessWidget {
     final buttonWidth = screenWidth * 0.35;
     const buttonHeight = 40.0;
 
-    return SingleChildScrollView(
-      padding: AppPadding.paddingMedium,
-      child: Column(
-        children: List.generate(
-          5,
-          (index) => QanonyAppointmentCardWidget(
-            name: 'الاء ايمن',
-            specialty: 'جنائيات',
-            description: 'اي وصف في الحياه تت تت',
-            price: '\$200',
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  CustomButton(
-                    text: 'قبول',
-                    onTap: () {},
-                    width: buttonWidth,
-                    height: buttonHeight,
-                    backgroundColor: AppColor.green,
-                    textStyle: AppText.bodyMedium,
-                  ),
-                  CustomButton(
-                    text: 'رفض',
-                    onTap: () {},
-                    width: buttonWidth,
-                    height: buttonHeight,
-                    backgroundColor: AppColor.primary,
-                    textStyle: AppText.bodyMedium,
-                  ),
-                ],
+    return BlocProvider(
+      create: (context) => OrderCubit()..loadMyOrders(),
+      child: BlocBuilder<OrderCubit, OrderState>(
+        builder: (context, state) {
+          if (state is OrderLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is OrderError) {
+            return Center(child: Text(state.message));
+          } else if (state is OrderLoaded) {
+            final pendingOrders = state.orders
+                .where((order) => order.status == OrderStatus.pending)
+                .toList();
+
+            if (pendingOrders.isEmpty) {
+              return const Center(child: Text('لا يوجد طلبات حالياً'));
+            }
+
+            return SingleChildScrollView(
+              padding: AppPadding.paddingMedium,
+              child: Column(
+                children: pendingOrders.map((order) {
+                  return QanonyAppointmentCardWidget(
+                    name: order.userName,
+                    specialty: order.caseType,
+                    description: order.caseDescription,
+                    price: '${order.price} EGP',
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          CustomButton(
+                            text: 'قبول',
+                            onTap: () async {
+                              await context
+                                  .read<OrderCubit>()
+                                  .updateOrderStatus(
+                                    order.orderId,
+                                    'accepted_by_lawyer',
+                                  );
+
+                              if (context.mounted) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text("تم قبول الطلب بنجاح"),
+                                  ),
+                                );
+
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const MyAppointmentsTab(),
+                                  ),
+                                );
+                              }
+                            },
+                            width: buttonWidth,
+                            height: buttonHeight,
+                            backgroundColor: AppColor.green,
+                            textStyle: AppText.bodyMedium,
+                          ),
+                          CustomButton(
+                            text: 'رفض',
+                            onTap: () async {
+                              await context
+                                  .read<OrderCubit>()
+                                  .updateOrderStatus(
+                                    order.orderId,
+                                    'rejected_by_lawyer',
+                                  );
+
+                              if (context.mounted) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(content: Text("تم رفض الطلب")),
+                                );
+                              }
+                            },
+                            width: buttonWidth,
+                            height: buttonHeight,
+                            backgroundColor: AppColor.primary,
+                            textStyle: AppText.bodyMedium,
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                }).toList(),
               ),
-            ],
-          ),
-        ),
+            );
+          }
+
+          return const SizedBox.shrink();
+        },
       ),
     );
   }
---
+++ lib/services/cubits/order_lawyer/order_cubit.dart
@@ -0,0 +1,47 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qanony/data/models/order_model.dart';
+import 'package:qanony/services/firestore/order_firestore_service.dart';
+
+part 'order_state.dart';
+
+class OrderCubit extends Cubit<OrderState> {
+  final OrderFirestoreService _orderService = OrderFirestoreService();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  OrderCubit() : super(OrderInitial());
+
+  void loadMyOrders() {
+    emit(OrderLoading());
+
+    final currentUser = _auth.currentUser;
+
+    if (currentUser == null) {
+      emit(OrderError("لم يتم تسجيل الدخول"));
+      return;
+    }
+
+    final myId = currentUser.uid;
+    print("🔥 My UID: $myId");
+    _orderService
+        .streamOrdersByLawyerId(myId)
+        .listen(
+          (orders) {
+            emit(OrderLoaded(orders));
+          },
+          onError: (error) {
+            emit(OrderError('فشل في تحميل الطلبات'));
+          },
+        );
+  }
+
+  Future<void> updateOrderStatus(String orderId, String newStatus) async {
+    try {
+      await _orderService.updateOrder(orderId, {'status': newStatus});
+      loadMyOrders();
+    } catch (e) {
+      emit(OrderError('فشل في تحديث الحالة'));
+    }
+  }
+}
