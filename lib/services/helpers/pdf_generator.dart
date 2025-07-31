377d090ee0cf2c454c27f488c7005a51719a4453
Amr Badran <badran.amr95@gmail.com>
2025-07-31 11:39:09 +0300

    fixed pdf generator

--- lib/services/helpers/pdf_generator.dart
+++ lib/services/helpers/pdf_generator.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/widgets.dart' as pw;
 
@@ -10,42 +11,49 @@ Future<String> generateSubscriptionPdf({
 }) async {
   final pdf = pw.Document();
 
+  final fontData = await rootBundle.load('assets/fonts/Cairo-Regular.ttf');
+  final ttf = pw.Font.ttf(fontData.buffer.asByteData());
+
   pdf.addPage(
     pw.Page(
       build: (context) {
-        return pw.Column(
-          crossAxisAlignment: pw.CrossAxisAlignment.start,
-          children: [
-            pw.Center(
-              child: pw.Text(
-                'تفاصيل الاشتراك',
-                style: pw.TextStyle(
-                  fontSize: 24,
-                  fontWeight: pw.FontWeight.bold,
+        return pw.Directionality(
+          textDirection: pw.TextDirection.rtl,
+          child: pw.Column(
+            crossAxisAlignment: pw.CrossAxisAlignment.start,
+            children: [
+              pw.Center(
+                child: pw.Text(
+                  'تفاصيل الاشتراك',
+                  style: pw.TextStyle(
+                    font: ttf,
+                    fontSize: 24,
+                    fontWeight: pw.FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            pw.SizedBox(height: 20),
-            pw.Text(
-              'الباقة الحالية: $subscriptionType',
-              style: pw.TextStyle(fontSize: 18),
-            ),
-            pw.SizedBox(height: 10),
-            pw.Text(
-              'تاريخ الاشتراك: ${subscriptionStart.toLocal().toString().split(' ')[0]}',
-              style: pw.TextStyle(fontSize: 18),
-            ),
-            pw.SizedBox(height: 10),
-            pw.Text(
-              'تاريخ الانتهاء: ${subscriptionEnd.toLocal().toString().split(' ')[0]}',
-              style: pw.TextStyle(fontSize: 18),
-            ),
-            pw.SizedBox(height: 10),
-            pw.Text(
-              'وسيلة الدفع: $paymentMethod',
-              style: pw.TextStyle(fontSize: 18),
-            ),
-          ],
+              pw.SizedBox(height: 20),
+              pw.Text(
+                'الباقة الحالية: $subscriptionType',
+                style: pw.TextStyle(font: ttf, fontSize: 18),
+              ),
+              pw.SizedBox(height: 10),
+              pw.Text(
+                'تاريخ الاشتراك: ${subscriptionStart.toLocal().toString().split(' ')[0]}',
+                style: pw.TextStyle(font: ttf, fontSize: 18),
+              ),
+              pw.SizedBox(height: 10),
+              pw.Text(
+                'تاريخ الانتهاء: ${subscriptionEnd.toLocal().toString().split(' ')[0]}',
+                style: pw.TextStyle(font: ttf, fontSize: 18),
+              ),
+              pw.SizedBox(height: 10),
+              pw.Text(
+                'وسيلة الدفع: $paymentMethod',
+                style: pw.TextStyle(font: ttf, fontSize: 18),
+              ),
+            ],
+          ),
         );
       },
     ),
